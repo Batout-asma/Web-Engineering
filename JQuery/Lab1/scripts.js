dd0072b76f9ef28b5931011bd26fba1de72f807d
Batoutiva <benramoulasma@gmail.com>
2024-11-05 14:03:22 +0100

    jquery javascript

---
+++ JQuery/Lab1/scripts.js
@@ -0,0 +1,35 @@
+$(document).ready(function () {
+  function calculate() {
+    let total = 0;
+    $(".quantity").each(function (index) {
+      const unitCost = parseFloat($(".unit-cost").eq(index).text());
+      const quantity = parseFloat($(this).val());
+      const cost = unitCost * quantity;
+      $("#cost-" + index).text(cost.toFixed(2));
+      total += cost;
+    });
+
+    const gst = total * 0.15;
+
+    $("#total").text(total.toFixed(2));
+    $("#gst").text(gst.toFixed(2));
+  }
+
+  function calculateGrandTotal() {
+    const total = parseFloat($("#total").text());
+    const gst = parseFloat($("#gst").text());
+    const grandTotal = total + gst;
+
+    $("#grand-total").text(grandTotal.toFixed(2));
+  }
+
+  $(".quantity").on("input", function () {
+    calculate();
+  });
+
+  $("#update-grand-total").on("click", function () {
+    calculateGrandTotal();
+  });
+
+  calculate();
+});
