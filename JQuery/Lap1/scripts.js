8e39acaab13b9fb1ff8341e17344e908f935c1f3
SAMAR-QUEEN-BOSS <samaryasmine20@gmail.com>
2024-11-05 01:03:57 +0100

    edit

--- JQuery/Lap1/scripts.js
+++ JQuery/Lap1/scripts.js
@@ -3,31 +3,46 @@ $(document).ready(function(){
     $("#Num_p1").text("1")
     $("#Nom_p1").text("Erithromycin 100 mg")
     $("#Prix_unit_p1").text("58")
-    $("#Quantity_p1").text("test")
-    $("#Cost_p1").text("test")
 
     $("#Num_p2").text("2")
     $("#Nom_p2").text("Femramycin 500 mg")
     $("#Prix_unit_p2").text("46")
-    $("#Quantity_p2").text("test")
-    $("#Cost_p2").text("test")
 
     $("#Num_p3").text("3")
     $("#Nom_p3").text("Oximycin 2 mg")
     $("#Prix_unit_p3").text("58")
-    $("#Quantity_p3").text("test")
-    $("#Cost_p3").text("test")
 
     $("#total_lettre").text("Total")
-    $("#total_chiffre").text("test")
 
     $("#gst_lettre").text("GST ot 15%")
-    $("#gst_chiffre").text("test")
 
-    $("#G-total_lettre").text("Grand Total Rs.")
-    $("#G-total_chiffre").text("test")
+    $("#G_total_lettre").text("Grand Total Rs.")
 
-    $("#Update").click(function(){
+$("#Update").click(function(){
+    
+    var Prix_unit_p1 =parseFloat($("#Prix_unit_p1").text());
+    var Prix_unit_p2 =parseFloat($("#Prix_unit_p2").text());
+    var Prix_unit_p3 =parseFloat($("#Prix_unit_p3").text());
+    var Quantity_p1 =parseFloat($("#Quantity_p1").val());
+    var Quantity_p1 =parseFloat($("#Quantity_p2").val());
+    var Quantity_p3 =$parseFloat(("#Quantity_p3").val());
 
-    })
-})
+    var Cost_p1 = Prix_unit_p1 * Quantity_p1;
+    $("#Cost_p1").text(Cost_p1);
+
+    var Cost_p2 = Prix_unit_p2 * Quantity_p2;
+    $("#Cost_p2").text(Cost_p2);
+
+    var Cost_p3 = Prix_unit_p3 * Quantity_p3;
+    $("#Cost_p3").text(Cost_p3);
+
+    var total_chiffre = Cost_p1 + Cost_p2 + Cost_p3;
+    $("#total_chiffre").text(total_chiffre.toFixed(2));
+
+    var gst_chiffre = (total_chiffre*15)/100;
+    $("#gst_chiffre").text(gst_chiffre.toFixed(2));
+
+    var G_total_chiffre = total_chiffre + gst_chiffre;
+    $("#G_total_chiffre").text(G_total_chiffre.toFixed(2));
+});
+});
