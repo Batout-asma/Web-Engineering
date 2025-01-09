14729dbd17eeb865d168b026ac64cd5a1691d16e
SAMAR-QUEEN-BOSS <samaryasmine20@gmail.com>
2025-01-09 02:12:29 +0100

    laravel & wordpress

---
+++ Laravel/lab2 Model & Migrations/database/migrations/2024_12_16_211809_create_articles_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateArticlesTable extends Migration
+{
+    /**
+     * Exécute les migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('content');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Annule les migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+}
